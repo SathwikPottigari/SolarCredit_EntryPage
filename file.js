b49d7c8cf7cb951ac645185ed294b9e90062b96a
Sathwik Pottigari <pottigarisathwik@gmail.com>
2026-02-01 13:04:55 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,10 @@
+function Button_1(){
+    document.getElementById('Button_1').onclick = function() { window.location.href = "https://github.com/Sai-dev313/sun-share-save"; }
+}
+
+function Button_2(){
+    document.getElementById('Button_2').onclick = function() { window.location.href = "https://sun-share-save.lovable.app"; }
+}
+function Button_3(){
+    document.getElementById('Button_3').onclick = function() { window.location.href = "https://drive.google.com/file/d/1kbRG-vHB5STkaTgVyrkXEWPMiL8wcAV6/view?usp=sharing"; }
+}
